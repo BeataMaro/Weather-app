f4cfcd90439c99316927dba647e159cc6a52bcb2
BeataMaro <beatamaro@interia.pl>
2023-02-28 20:56:21 +0100

    Updates

--- src/components/Container/Container.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StyledWrapper } from "./StyledContainer";
-
-const Container = (props) => {
-  return <StyledWrapper>{props.children}</StyledWrapper>;
-};
-
-export default Container;
--- src/utils/roll.js
+++
@@ -1,4 +0,0 @@
-export const roll = (min, max, floatFlag) => {
-  let r = Math.random() * (max - min) + min;
-  return floatFlag ? r : Math.floor(r);
-};
